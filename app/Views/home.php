7556716f1421dedef286f1e072d735fd25671905
RivaldiSiby <rivaldisiby@gmail.com>
2021-10-28 23:54:37 +0800

    melakukan penyesuaian

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,6 +1,6 @@
 <?= $this->extend('layout/template'); ?>
 <?= $this->section('content'); ?>
 <?= $this->include('layout/nav'); ?>
-Hi guys
+Hi guys selamat datang
 <a href="/hal">Halaman 2</a>
 <?= $this->endSection(); ?>
